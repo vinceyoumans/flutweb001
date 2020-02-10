716ed8e8b9b7acfbf29696620badf8fac1205f18
vyoumans <vinceyoumans@gmail.com>
2020-02-09 21:45:26 -0500

    com 1

---
+++ lib/widgets/course_details/course_details.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+
+class CourseDetails extends StatelessWidget {
+  const CourseDetails({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveBuilder(
+      builder: (context, sizingInformation) {
+        var textAlignment =
+            sizingInformation.deviceScreenType == DeviceScreenType.Desktop
+                ? TextAlign.left
+                : TextAlign.center;
+        double titleSize =
+            sizingInformation.deviceScreenType == DeviceScreenType.Mobile
+                ? 50
+                : 80;
+
+        double descriptionSize =
+            sizingInformation.deviceScreenType == DeviceScreenType.Mobile
+                ? 16
+                : 21;
+
+        return Container(
+          width: 600,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'FLUTTER WEB.\nTHE BASICS',
+                style: TextStyle(
+                    fontWeight: FontWeight.w800,
+                    height: 0.9,
+                    fontSize: titleSize),
+                textAlign: textAlignment,
+              ),
+              SizedBox(
+                height: 30,
+                //child: const Card(child: Text('Hello World!')),
+              ),
+              SizedBox(
+                  // child: Text(
+                  //   'In this course we will go over the basics of using Flutter Web for website development. Topics will include Responsive Layout, Deploying, Font Changes, Hover Functionality, Modals and more.',
+                  //   style: TextStyle(
+                  //     fontSize: descriptionSize,
+                  //     height: 1.7,
+                  //   ),
+                  //   textAlign: textAlignment,
+                  // ),
+                  ),
+              Text(
+                'In this course we will go over the basics of using Flutter Web for website development. Topics will include Responsive Layout, Deploying, Font Changes, Hover Functionality, Modals and more.',
+                style: TextStyle(
+                  fontSize: descriptionSize,
+                  height: 1.7,
+                ),
+                textAlign: textAlignment,
+                //SingleChildScrollView
+              ),
+              Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const ListTile(
+                      leading: Icon(Icons.album, size: 50),
+                      title: Text('Heart Shaker'),
+                      subtitle: Text('TWICE'),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
